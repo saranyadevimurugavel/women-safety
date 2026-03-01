31c2d1a22d94807445270ad2efaf71e309e6eb8c
saranyadevimurugavel <saranyadevimurugavel1426@gmail.com>
2026-03-01 23:02:05 +0530

    Update vite.config.ts

--- src/vite.config.ts
+++ src/vite.config.ts
@@ -1,1 +1,7 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
 
+export default defineConfig({
+  base: '/women-safety/',  // ⚠️ your repo name
+  plugins: [react()],
+})
